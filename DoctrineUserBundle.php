b44440ad40423824b993898ef61d92d5f2665516
ornicar <thibault.duplessis@gmail.com>
2010-06-25 17:50:46 +0200

    Add DoctrineUserBundle

---
+++ DoctrineUserBundle.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the Symfony framework.
+ *
+ * (c) Matthieu Bontemps <matthieu@knplabs.com>
+ * (c) Thibault Duplessis <thibault.duplessis@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Bundle\DoctrineUserBundle;
+
+use Symfony\Foundation\Bundle\Bundle as BaseBundle;
+
+use Symfony\Components\DependencyInjection\ContainerInterface;
+use Symfony\Components\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Components\DependencyInjection\BuilderConfiguration;
+
+class DoctrineUserBundle extends BaseBundle
+{
+    public function buildContainer(ContainerInterface $container)
+    {
+        $configuration = new BuilderConfiguration();
+        $loader = new XmlFileLoader(__DIR__.'/Resources/config');
+        $configuration->merge($loader->load('listener.xml'));
+        $configuration->merge($loader->load('controller.xml'));
+
+        return $configuration;
+    }
+
+    public function boot(ContainerInterface $container)
+    {
+        $container->getDoctrineUserAuthListenerService();
+    }
+}
