2eb408ba9a1c4087730f631c723556b7e85ec7b1
ornicar <thibault.duplessis@gmail.com>
2010-08-07 14:50:55 +0200

    Move repository interface to DAO

--- DAO/UserRepositoryInterface.php
+++ DAO/UserRepositoryInterface.php
@@ -3,14 +3,14 @@
 /**
  * This file is part of the Symfony framework.
  *
- * (c) Matthieu Bontemps <matthieu@knplabs.com>
+ * (c) Gordon Franke <info@nevalon.de>
  * (c) Thibault Duplessis <thibault.duplessis@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 
-namespace Bundle\DoctrineUserBundle\Entity;
+namespace Bundle\DoctrineUserBundle\DAO;
 
 interface UserRepositoryInterface
 {
@@ -20,6 +20,7 @@ interface UserRepositoryInterface
      * @param string  $password       password
      * @param boolean $isActive      is the user active
      * @param boolean $isSuperAdmin is the user a super admin
+     * @return  BaseUser The created user
      */
     public function createUser($username, $password, $isActive = true, $isSuperAdmin = false);
 
@@ -27,14 +28,14 @@ interface UserRepositoryInterface
      * Find a user by its username and password
      * @param   string  $username
      * @param   string  $password
-     * @return  User or null if user does not exist
+     * @return  BaseUser or null if user does not exist
      */
     public function findOneByUsernameAndPassword($username, $password);
 
     /**
      * Find a user by its username
      * @param   string  $username
-     * @return  User or null if user does not exist
+     * @return  BaseUser or null if user does not exist
      */
     public function findOneByUsername($username);
 }
