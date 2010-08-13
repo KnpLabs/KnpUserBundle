b7f56a9242f3fb7ce80c272fe97bb84734d558ed
ornicar <thibault.duplessis@gmail.com>
2010-08-13 23:27:10 +0200

    Add ODM implementation of the UserRepository

---
+++ Document/UserRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Symfony framework.
+ *
+ * (c) Matthieu Bontemps <matthieu@knplabs.com>
+ * (c) Thibault Duplessis <thibault.duplessis@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Bundle\DoctrineUserBundle\Document;
+use Doctrine\ODM\MongoDB\DocumentRepository;
+use Bundle\DoctrineUserBundle\DAO\UserRepositoryInterface;
+
+class UserRepository extends DocumentRepository implements UserRepositoryInterface
+{
+    /**
+     * @see UserRepositoryInterface::findOneByUsernameAndPassword
+     */
+    public function findOneByUsernameAndPassword($username, $password)
+    {
+        $user = $this->findOneByUsername($username);
+        
+        if($user && $user->checkPassword($password))
+        {
+            return $user;
+        }
+    }
+
+    /**
+     * @see UserRepositoryInterface::findOneByUsername
+     */
+    public function findOneByUsername($username)
+    {
+        return $this->findOneBy(array('username' => $username));
+    }
+
+}
