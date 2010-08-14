aeac4fa206ffb3715897b72cdebd1bd3c31c76bb
ornicar <thibault.duplessis@gmail.com>
2010-08-14 14:25:59 +0200

    Add User tests

---
+++ Tests/DAO/UserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Bundle\DoctrineUserBundle\Tests\DAO;
+
+use Bundle\DoctrineUserBundle\DAO\User as AbstractUser;
+
+class User extends AbstractUser
+{
+}
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTimestampable()
+    {
+        $user = new User();
+        
+        $this->assertTrue($user->getCreatedAt() instanceof \DateTime);
+        $this->assertEquals(new \DateTime(), $user->getCreatedAt());
+        
+        $this->assertTrue($user->getUpdatedAt() instanceof \DateTime);
+        $this->assertEquals(new \DateTime(), $user->getUpdatedAt());
+    }
+
+    public function testPassword()
+    {
+        $user = new User();
+        $user->setPassword('changeme');
+
+        $this->assertFalse($user->checkPassword('badpassword'));
+        $this->assertTrue($user->checkPassword('changeme'));
+    }
+
+    public function testUsername()
+    {
+        $user = new User();
+        $this->assertNull($user->getUsername());
+        
+        $user->setUsername('tony');
+        $this->assertEquals('tony', $user->getUsername());
+    }
+}
