60e1cd5f2564c3f3de7759bbf5fe6e10a1c89821
ornicar <thibault.duplessis@gmail.com>
2010-08-19 17:16:27 +0200

    Add PromoteSuperAdmin command with tests

---
+++ Command/PromoteSuperAdminCommand.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Bundle\DoctrineUserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\Command as BaseCommand;
+use Symfony\Components\Console\Input\InputArgument;
+use Symfony\Components\Console\Input\InputOption;
+use Symfony\Components\Console\Input\InputInterface;
+use Symfony\Components\Console\Output\OutputInterface;
+use Symfony\Components\Console\Output\Output;
+use Bundle\DoctrineUserBundle\Entity\User;
+
+/*
+ * This file is part of the DoctrineUserBundle
+ *
+ * (c) Matthieu Bontemps <matthieu@knplabs.com>
+ * (c) Thibault Duplessis <thibault.duplessis@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * CreateUserCommand.
+ *
+ * @package    Bundle
+ * @subpackage DoctrineUserBundle
+ * @author     Matthieu Bontemps <matthieu@knplabs.com>
+ * @author     Thibault Duplessis <thibault.duplessis@gmail.com>
+ */
+class PromoteSuperAdminCommand extends BaseCommand
+{
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('doctrine:user:promote')
+            ->setDescription('Promotes a user as a super administrator')
+            ->setDefinition(array(
+                new InputArgument('username', InputArgument::REQUIRED, 'The username'),
+            ))
+            ->setHelp(<<<EOT
+The <info>doctrine:user:promote</info> command promotes a user as a super administrator
+
+  <info>php app/console doctrine:user:promote matthieu</info>
+EOT
+        );
+    }
+
+    /**
+     * @see Command
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userRepo = $this->container->get('doctrine_user.user_repository');
+        $user = $userRepo->findOneByUsername($input->getArgument('username'));
+
+        if(!$user) {
+            throw new \InvalidArgumentException(sprintf('The user "%s" does not exist', $input->getArgument('username')));
+        }
+        $user->setIsSuperAdmin(true);
+       
+        $userRepo->getObjectManager()->persist($user);
+        $userRepo->getObjectManager()->flush();
+
+        $output->writeln(sprintf('User "%s" has been promoted as a super administrator.', $user->getUsername()));
+    }
+    
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if(!$input->getArgument('username')) {
+            $username = $this->getHelper('dialog')->askAndValidate(
+                $output,
+                'Please choose a username:',
+                function($username) {
+                    if(empty($username)) {
+                        throw new \Exception('Username can not be empty');
+                    }
+                    return $username;
+                }
+            );
+            $input->setArgument('username', $username);
+        }
+    }
+}
