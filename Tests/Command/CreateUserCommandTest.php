122b7f600da17c645989d5df74874c46b9f9e5b4
ornicar <thibault.duplessis@gmail.com>
2010-08-19 15:40:44 +0200

    Refactor test classes

--- Tests/Command/CreateUserCommandTest.php
+++ Tests/Command/CreateUserCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Bundle\DoctrineUserBundle\Tests\Command;
 
+use Bundle\DoctrineUserBundle\Tests\BaseDatabaseTest;
 use Bundle\DoctrineUserBundle\DAO\User;
 use Bundle\DoctrineUserBundle\Command\CreateUserCommand;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
@@ -11,7 +12,7 @@ use Symfony\Components\Console\Tester\ApplicationTester;
 // Kernel creation required namespaces
 use Symfony\Components\Finder\Finder;
 
-class CreateUserCommandTest extends \PHPUnit_Framework_TestCase
+class CreateUserCommandTest extends BaseDatabaseTest
 {
     public function testUserCreation()
     {
@@ -50,63 +51,5 @@ class CreateUserCommandTest extends \PHPUnit_Framework_TestCase
             $objectManager->remove($object);
         }
         $objectManager->flush();
-    }
-
-    /**
-     * Creates a Kernel.
-     *
-     * If you run tests with the PHPUnit CLI tool, everything will work as expected.
-     * If not, override this method in your test classes.
-     *
-     * Available options:
-     *
-     *  * environment
-     *  * debug
-     *
-     * @param array $options An array of options
-     *
-     * @return HttpKernelInterface A HttpKernelInterface instance
-     */
-    static protected function createKernel(array $options = array())
-    {
-        // black magic below, you have been warned!
-        $dir = getcwd();
-        if (!isset($_SERVER['argv']) || false === strpos($_SERVER['argv'][0], 'phpunit')) {
-            throw new \RuntimeException('You must override the WebTestCase::createKernel() method.');
-        }
-
-        // find the --configuration flag from PHPUnit
-        $cli = implode(' ', $_SERVER['argv']);
-        if (preg_match('/\-\-configuration[= ]+([^ ]+)/', $cli, $matches)) {
-            $dir = $dir.'/'.$matches[1];
-        } elseif (preg_match('/\-c +([^ ]+)/', $cli, $matches)) {
-            $dir = $dir.'/'.$matches[1];
-        } else {
-            throw new \RuntimeException('Unable to guess the Kernel directory.');
-        }
-
-        if (!is_dir($dir)) {
-            $dir = dirname($dir);
-        }
-
-        $finder = new Finder();
-        $finder->name('*Kernel.php')->in($dir);
-        if (!count($finder)) {
-            throw new \RuntimeException('You must override the WebTestCase::createKernel() method.');
-        }
-
-        $file = current(iterator_to_array($finder));
-        $class = $file->getBasename('.php');
-        unset($finder);
-
-        require_once $file;
-
-        $kernel = new $class(
-            isset($options['environment']) ? $options['environment'] : 'test',
-            isset($options['debug']) ? $debug : true
-        );
-        $kernel->boot();
-
-        return $kernel;
     }
 }
--- Tests/DAO/UserRepositoryTest.php
+++ Tests/DAO/UserRepositoryTest.php
@@ -2,13 +2,14 @@
 
 namespace Bundle\DoctrineUserBundle\Tests\DAO;
 
+use Bundle\DoctrineUserBundle\Tests\BaseDatabaseTest;
 use Bundle\DoctrineUserBundle\DAO\User;
 use Bundle\DoctrineUserBundle\DAO\UserRepositoryInterface;
 
 // Kernel creation required namespaces
 use Symfony\Components\Finder\Finder;
 
-class UserRepositoryTest extends \PHPUnit_Framework_TestCase
+class UserRepositoryTest extends BaseDatabaseTest
 {
     public function testGetUserRepo()
     {
@@ -135,63 +136,5 @@ class UserRepositoryTest extends \PHPUnit_Framework_TestCase
             }
         }
         $objectManager->flush();
-    }
-
-    /**
-     * Creates a Kernel.
-     *
-     * If you run tests with the PHPUnit CLI tool, everything will work as expected.
-     * If not, override this method in your test classes.
-     *
-     * Available options:
-     *
-     *  * environment
-     *  * debug
-     *
-     * @param array $options An array of options
-     *
-     * @return HttpKernelInterface A HttpKernelInterface instance
-     */
-    static protected function createKernel(array $options = array())
-    {
-        // black magic below, you have been warned!
-        $dir = getcwd();
-        if (!isset($_SERVER['argv']) || false === strpos($_SERVER['argv'][0], 'phpunit')) {
-            throw new \RuntimeException('You must override the WebTestCase::createKernel() method.');
-        }
-
-        // find the --configuration flag from PHPUnit
-        $cli = implode(' ', $_SERVER['argv']);
-        if (preg_match('/\-\-configuration[= ]+([^ ]+)/', $cli, $matches)) {
-            $dir = $dir.'/'.$matches[1];
-        } elseif (preg_match('/\-c +([^ ]+)/', $cli, $matches)) {
-            $dir = $dir.'/'.$matches[1];
-        } else {
-            throw new \RuntimeException('Unable to guess the Kernel directory.');
-        }
-
-        if (!is_dir($dir)) {
-            $dir = dirname($dir);
-        }
-
-        $finder = new Finder();
-        $finder->name('*Kernel.php')->in($dir);
-        if (!count($finder)) {
-            throw new \RuntimeException('You must override the WebTestCase::createKernel() method.');
-        }
-
-        $file = current(iterator_to_array($finder));
-        $class = $file->getBasename('.php');
-        unset($finder);
-
-        require_once $file;
-
-        $kernel = new $class(
-            isset($options['environment']) ? $options['environment'] : 'test',
-            isset($options['debug']) ? $debug : true
-        );
-        $kernel->boot();
-
-        return $kernel;
     }
 }
