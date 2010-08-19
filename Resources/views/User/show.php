55bfaa85670f17af693809fcac21b091b3e60266
ornicar <thibault.duplessis@gmail.com>
2010-08-19 21:23:23 +0200

    Complete user show implementation

---
+++ Resources/views/User/show.php
@@ -0,0 +1,10 @@
+<?php $view->extend('DoctrineUserBundle::layout') ?>
+
+<?php if ($view->session->hasFlash('doctrine_user_user_create/success')): ?>
+<div class="doctrine_user_user_create_success">The user has been created successfully</div>
+<?php endif; ?>
+
+<div class="doctrine_user_user_show">
+    <p>Username: <?php echo $user->getUsername() ?></p>
+    <p>Email: <?php echo $user->getEmail() ?></p>
+</div>
