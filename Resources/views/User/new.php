c2fd3ecec42594a7d06266817bd379047ade512b
Antoine Hérault <antoine.herault@gmail.com>
2010-08-20 14:36:50 +0200

    modified user creation form view

--- Resources/views/User/new.php
+++ Resources/views/User/new.php
@@ -2,17 +2,17 @@
 
 <?php echo $form->getRawValue()->renderFormTag($view->router->generate('doctrine_user_user_create'), array('class' => 'doctrine_user_user_new')) ?>
     <div>
-        <label for="doctrine_user_user_new_username">Username:</label>
+        <label for="<?php $form['username']->getId() ?>">Username:</label>
         <?php echo $form['username']->getRawValue()->render(); ?>
         <?php echo $form['username']->getRawValue()->renderErrors() ?>
     </div>
     <div>
-        <label for="doctrine_user_user_new_email">Email:</label>
+        <label for="<?php echo $form['email']->getId() ?>">Email:</label>
         <?php echo $form['email']->getRawValue()->render(); ?>
         <?php echo $form['email']->getRawValue()->renderErrors() ?>
     </div>
     <div>
-        <label for="doctrine_user_user_new_password">Password:</label>
+        <label for="<?php echo $form['password']->getId() ?>">Password:</label>
         <?php echo $form['password']->getRawValue()->render(); ?>
         <?php echo $form['password']->getRawValue()->renderErrors() ?>
     </div>
