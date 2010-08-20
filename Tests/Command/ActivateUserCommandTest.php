003a8008b560f8864172c94a8dc7ce36e8f9682b
Antoine Hérault <antoine.herault@gmail.com>
2010-08-20 14:55:15 +0200

    added (de)activation and demote commands

---
+++ Tests/Command/ActivateUserCommandTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Bundle\DoctrineUserBundle\Tests\Command;
+
+use Bundle\DoctrineUserBundle\Tests\BaseDatabaseTest;
+use Bundle\DoctrineUserBundle\DAO\User;
+use Bundle\DoctrineUserBundle\Command\CreateUserCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Components\Console\Output\Output;
+use Symfony\Components\Console\Tester\ApplicationTester;
+
+// Kernel creation required namespaces
+use Symfony\Components\Finder\Finder;
+
+class ActivateUserCommandTest extends BaseDatabaseTest
+{
+    public function testUserActivation()
+    {
+        $kernel = self::createKernel1();
+        $command = new ActivateUserCommand();
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+        $tester = new ApplicationTester($application);
+
+        $username = 'test_username';
+        $password = 'test_password';
+        $email    = 'test_email@email.org';
+
+        $userRepo = $kernel->getContainer()->get('doctrine_user.user_repository');
+        $userClass = $userRepo->getObjectClass();
+
+        $user = new $userClass();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPassword($password);
+        $user->setIsActive(false);
+
+        $userRepo->getObjectManager()->persist($user);
+        $userRepo->getObjectManager()->flush();
+        
+        $this->assertFalse($user->getIsActive());
+
+        $tester->run(array(
+            'command'  => $command->getFullName(),
+            'username' => $username,
+        ), array('interactive' => false, 'decorated' => false, 'verbosity' => Output::VERBOSITY_VERBOSE));
+
+        $kernel = self::createKernel();
+        $userRepo = $kernel->getContainer()->get('doctrine_user.user_repository');
+        $user = $userRepo->findOneByUsername($username);
+
+        $this->assertTrue($user instanceof User);
+        $this->assertTrue($user->getIsActive());
+
+        $userRepo->getObjectManager()->remove($user);
+        $userRepo->getObjectManager()->flush();
+    }
+
+
+    static public function tearDownAfterClass()
+    {
+        $userRepo = self::createKernel()->getContainer()->getDoctrineUser_UserRepositoryService();
+        $objectManager = $userRepo->getObjectManager();
+        if($object = $userRepo->findOneByUsername('test_username')) {
+            $objectManager->remove($object);
+        }
+        $objectManager->flush();
+    }
+}
---
+++ Tests/Command/DeactivateUserCommandTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Bundle\DoctrineUserBundle\Tests\Command;
+
+use Bundle\DoctrineUserBundle\Tests\BaseDatabaseTest;
+use Bundle\DoctrineUserBundle\DAO\User;
+use Bundle\DoctrineUserBundle\Command\CreateUserCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Components\Console\Output\Output;
+use Symfony\Components\Console\Tester\ApplicationTester;
+
+// Kernel creation required namespaces
+use Symfony\Components\Finder\Finder;
+
+class DeactivateUserCommandTest extends BaseDatabaseTest
+{
+    public function testUserDeactivation()
+    {
+        $kernel = self::createKernel1();
+        $command = new DeactivateUserCommand();
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+        $tester = new ApplicationTester($application);
+
+        $username = 'test_username';
+        $password = 'test_password';
+        $email    = 'test_email@email.org';
+
+        $userRepo = $kernel->getContainer()->get('doctrine_user.user_repository');
+        $userClass = $userRepo->getObjectClass();
+
+        $user = new $userClass();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPassword($password);
+
+        $userRepo->getObjectManager()->persist($user);
+        $userRepo->getObjectManager()->flush();
+        
+        $this->assertFalse($user->getIsActive());
+
+        $tester->run(array(
+            'command'  => $command->getFullName(),
+            'username' => $username,
+        ), array('interactive' => false, 'decorated' => false, 'verbosity' => Output::VERBOSITY_VERBOSE));
+
+        $kernel = self::createKernel();
+        $userRepo = $kernel->getContainer()->get('doctrine_user.user_repository');
+        $user = $userRepo->findOneByUsername($username);
+
+        $this->assertTrue($user instanceof User);
+        $this->assertFalse($user->getIsActive());
+
+        $userRepo->getObjectManager()->remove($user);
+        $userRepo->getObjectManager()->flush();
+    }
+
+
+    static public function tearDownAfterClass()
+    {
+        $userRepo = self::createKernel()->getContainer()->getDoctrineUser_UserRepositoryService();
+        $objectManager = $userRepo->getObjectManager();
+        if($object = $userRepo->findOneByUsername('test_username')) {
+            $objectManager->remove($object);
+        }
+        $objectManager->flush();
+    }
+}
