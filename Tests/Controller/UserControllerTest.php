8052c4d8745cecc2e43e57c7ccc87ddb372ade7d
ornicar <thibault.duplessis@gmail.com>
2010-08-21 14:26:51 +0200

    Complete User CRUD functional tests

--- Tests/Controller/UserControllerTest.php
+++ Tests/Controller/UserControllerTest.php
@@ -54,17 +54,14 @@ class UserControllerTest extends WebTestCase
         $this->assertRegexp('/harry_test/', $client->getResponse()->getContent());
     }
 
+    /**
+     * @depends testCreateSuccess
+     */
     public function testList()
     {
         $userRepo = self::staticCreateKernel()->getContainer()->getDoctrineUser_UserRepositoryService();
-        $objectManager = $userRepo->getObjectManager();
-
         $userClass = $userRepo->getObjectClass();
-        $user = new $userClass();
-        $user->setUserName('harry_test');
-        $user->setEmail('harry@mail.org');
-        $user->setPassword('changeme');
-        $objectManager->persist($user);
+        $objectManager = $userRepo->getObjectManager();
 
         $user2 = new $userClass();
         $user2->setUserName('harry_test2');
@@ -81,6 +78,90 @@ class UserControllerTest extends WebTestCase
         $this->assertRegexp('/harry_test2/', $client->getResponse()->getContent());
     }
 
+    /**
+     * @depends testCreateSuccess
+     */
+    public function testEdit()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_edit', array('username' => 'harry_test')));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('form.doctrine_user_user_edit')->count());
+    }
+
+    /**
+     * @depends testCreateSuccess
+     */
+    public function testUpdateSuccess()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_edit', array('username' => 'harry_test')));
+        $form = $crawler->selectButton('Update user')->form();
+        $client->submit($form, array('doctrine_user_user_edit[username]' => 'harry_test_modified', 'doctrine_user_user_edit[email]' => 'harry_test_modified@email.org', 'doctrine_user_user_edit[password]' => 'changeme'));
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('div.doctrine_user_user_update_success')->count());
+        $this->assertRegexp('/harry_test_modified/', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @depends testUpdateSuccess
+     */
+    public function testUpdateEmptyFormError()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_edit', array('username' => 'harry_test_modified')));
+        $form = $crawler->selectButton('Update user')->form();
+        $client->submit($form, array('doctrine_user_user_edit[username]' => '', 'doctrine_user_user_edit[email]' => '', 'doctrine_user_user_edit[password]' => ''));
+        $this->assertFalse($client->getResponse()->isRedirect());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $form = $crawler->filter('form.doctrine_user_user_edit');
+        $this->assertEquals(1, $form->count());
+        $this->assertRegexp('/This value should not be blank/', $client->getResponse()->getContent());
+        $this->assertEquals(0, $crawler->filter('div.doctrine_user_user_update_success')->count());
+    }
+
+    /**
+     * @depends testUpdateEmptyFormError
+     */
+    public function testUpdateEmptyFormErrorThenRetrySuccess()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_edit', array('username' => 'harry_test_modified')));
+        $form = $crawler->selectButton('Update user')->form();
+        $client->submit($form, array('doctrine_user_user_edit[username]' => '', 'doctrine_user_user_edit[email]' => '', 'doctrine_user_user_edit[password]' => ''));
+        $form = $crawler->selectButton('Update user')->form();
+        $client->submit($form, array('doctrine_user_user_edit[username]' => 'harry_test', 'doctrine_user_user_edit[email]' => 'harry_test@email.org', 'doctrine_user_user_edit[password]' => 'changeme'));
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('div.doctrine_user_user_update_success')->count());
+        $this->assertRegexp('/harry_test/', $client->getResponse()->getContent());
+    }
+
+    public function testDelete()
+    {
+        $userRepo = self::staticCreateKernel()->getContainer()->getDoctrineUser_UserRepositoryService();
+        $userClass = $userRepo->getObjectClass();
+        $objectManager = $userRepo->getObjectManager();
+
+        $user = new $userClass();
+        $user->setUserName('harry_test3');
+        $user->setEmail('harry3@mail.org');
+        $user->setPassword('changeme3');
+        $objectManager->persist($user);
+
+        $objectManager->flush();
+
+        $client = $this->createClient();
+        $crawler = $client->request('GET', $this->generateUrl($client, 'doctrine_user_user_delete', array('username' => 'harry_test3')));
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertNotRegexp('/harry_test3/', $client->getResponse()->getContent());
+    }
+
     protected function generateUrl($client, $route, array $params = array())
     {
         return $client->getContainer()->get('router')->generate($route, $params);
@@ -90,7 +171,7 @@ class UserControllerTest extends WebTestCase
     {
         $userRepo = self::staticCreateKernel()->getContainer()->getDoctrineUser_UserRepositoryService();
         $objectManager = $userRepo->getObjectManager();
-        foreach(array('harry_test', 'harry_test2') as $username) {
+        foreach(array('harry_test', 'harry_test2', 'harry_test_modified') as $username) {
             if($object = $userRepo->findOneByUsername($username)) {
                 $objectManager->remove($object);
             }
