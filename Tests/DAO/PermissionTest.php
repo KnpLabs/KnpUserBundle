ad8da71828c9e928052ab57f5252afb9f8cae5cf
ornicar <thibault.duplessis@gmail.com>
2010-08-23 00:09:25 +0200

    Add tests for Permission model object and repository

---
+++ Tests/DAO/PermissionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bundle\DoctrineUserBundle\Tests\DAO;
+
+use Bundle\DoctrineUserBundle\DAO\Permission as AbstractPermission;
+
+class Permission extends AbstractPermission
+{
+}
+
+class PermissionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testName()
+    {
+        $permission = new Permission();
+        $this->assertNull($permission->getName());
+        
+        $permission->setName('deletion');
+        $this->assertEquals('deletion', $permission->getName());
+    }
+
+    public function testDescription()
+    {
+        $permission = new Permission();
+        $this->assertNull($permission->getDescription());
+        
+        $permission->setDescription('Delete things');
+        $this->assertEquals('Delete things', $permission->getDescription());
+    }
+}
