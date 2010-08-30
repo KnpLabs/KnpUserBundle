205ed270393f1438a24727909a6eed748b5c0cd0
Antoine Hérault <antoine.herault@gmail.com>
2010-08-30 11:32:40 +0200

    refactored repositories

--- Document/GroupRepository.php
+++ Document/GroupRepository.php
@@ -2,10 +2,9 @@
 
 namespace Bundle\DoctrineUserBundle\Document;
 
-use Doctrine\ODM\MongoDB\DocumentRepository;
 use Bundle\DoctrineUserBundle\DAO\GroupRepositoryInterface;
 
-class GroupRepository extends DocumentRepository implements GroupRepositoryInterface
+class GroupRepository extends ObjectRepository implements GroupRepositoryInterface
 {
     /**
      * @see GroupRepositoryInterface::findOneByName
@@ -13,29 +12,5 @@ class GroupRepository extends DocumentRepository implements GroupRepositoryInter
     public function findOneByName($name)
     {
         return $this->findOneBy(array('name' => $name));
-    }
-    
-    /**
-     * @see GroupRepositoryInterface::getObjectManager
-     */
-    public function getObjectManager()
-    {
-        return $this->getDocumentManager();
-    }
-
-    /**
-     * @see GroupRepositoryInterface::getObjectClass
-     */
-    public function getObjectClass()
-    {
-        return $this->getDocumentName();
-    }
-
-    /**
-     * @see GroupRepositoryInterface::getObjectIdentifier
-     */
-    public function getObjectIdentifier()
-    {
-        return $this->getClassMetadata()->identifier;
     }
 }
--- Entity/PermissionRepository.php
+++ Entity/PermissionRepository.php
@@ -2,11 +2,10 @@
 
 namespace Bundle\DoctrineUserBundle\Entity;
 
-use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NoResultException;
 use Bundle\DoctrineUserBundle\DAO\PermissionRepositoryInterface;
 
-class PermissionRepository extends EntityRepository implements PermissionRepositoryInterface
+class PermissionRepository extends ObjectRepository implements PermissionRepositoryInterface
 {
     /**
      * @see PermissionRepositoryInterface::findOneByName
@@ -14,29 +13,5 @@ class PermissionRepository extends EntityRepository implements PermissionReposit
     public function findOneByName($name)
     {
         return $this->findOneBy(array('name' => $name));
-    }
-
-    /**
-     * @see PermissionRepositoryInterface::getObjectManager
-     */
-    public function getObjectManager()
-    {
-        return $this->getEntityManager();
-    }
-
-    /**
-     * @see PermissionRepositoryInterface::getObjectClass
-     */
-    public function getObjectClass()
-    {
-        return $this->getEntityName();
-    }
-
-    /**
-     * @see PermissionRepositoryInterface::getObjectIdentifier
-     */
-    public function getObjectIdentifier()
-    {
-        return reset($this->getClassMetadata()->identifier);
     }
 }
