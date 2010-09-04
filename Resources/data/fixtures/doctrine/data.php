bea44a947a419a3233c91845343f1d1629423121
ornicar <thibault.duplessis@gmail.com>
2010-09-04 00:05:44 -0700

    Add group and permission fixtures

--- Resources/data/fixtures/doctrine/data.php
+++ Resources/data/fixtures/doctrine/data.php
@@ -1,5 +1,29 @@
 <?php
 
+// Permissions
+
+$permissionClass = $this->container->get('doctrine_user.permission_repository')->getObjectClass();
+
+$nbPermissions = 5;
+for($it = 1; $it <= $nbPermissions; $it++) {
+    ${'permission'.$it} = new $permissionClass();
+    ${'permission'.$it}->setName('permission'.$it);
+    ${'permission'.$it}->setDescription('Permission number #'.$it);
+}
+
+// Groups
+
+$groupClass = $this->container->get('doctrine_user.group_repository')->getObjectClass();
+
+$nbGroups = 5;
+for($it = 1; $it <= $nbGroups; $it++) {
+    ${'group'.$it} = new $groupClass();
+    ${'group'.$it}->setName('group'.$it);
+    ${'group'.$it}->setDescription('Group number #'.$it);
+}
+
+// Users
+
 $userClass = $this->container->get('doctrine_user.user_repository')->getObjectClass();
 
 $admin = new $userClass();
@@ -8,9 +32,31 @@ $admin->setEmail('admin@site.org');
 $admin->setPassword('admin');
 $admin->setIsSuperAdmin(true);
 
-for($it = 1; $it <= 5; $it++) {
+$nbUsers = 5;
+for($it = 1; $it <= $nbUsers; $it++) {
     ${'user'.$it} = new $userClass();
     ${'user'.$it}->setUsername('user'.$it);
     ${'user'.$it}->setEmail('user'.$it.'@site.org');
     ${'user'.$it}->setPassword('password'.$it);
+}
+
+// User permissions
+
+$user1->addPermission($permission4);
+$user1->addPermission($permission5);
+
+// Group permissions
+
+for($it = 1; $it <= $nbPermissions; $it++) {
+    for($jt = 0; $jt <= 1; $jt++) {
+        ${'group'.$it}->addPermission(${'permission'.(($it+$jt)%$nbPermissions+1)});
+    }
+}
+
+// User groups
+
+for($it = 1; $it <= $nbUsers; $it++) {
+    for($jt = 0; $jt <= 1; $jt++) {
+        ${'user'.$it}->addGroup(${'group'.(($it+$jt)%$nbGroups+1)});
+    }
 }
