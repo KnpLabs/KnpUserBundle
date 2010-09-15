dbd8369a502d864b1c3ed0d1ec5585a8a8829fd1
ornicar <thibault.duplessis@gmail.com>
2010-09-14 17:58:26 -0700

    Make Auth service support remember_me

--- Auth.php
+++ Auth.php
@@ -17,12 +17,21 @@ class Auth
      * @var UserRepositoryInterface
      */
     protected $userRepository = null;
+
+    /**
+     * The Symfony2 Request service
+     *
+     * @var Request
+     */
+    protected $request = null;
+
     /**
      * The Symfony2 Session service
      *
      * @var Session
      */
     protected $session = null;
+
     /**
      * The user object bound to the session, if any
      *
@@ -34,32 +43,41 @@ class Auth
      * Instanciate the Auth service 
      * 
      * @param UserRepositoryInterface $userRepository 
-     * @param Session $session 
+     * @param Request $request 
      */
-    public function __construct(UserRepositoryInterface $userRepository, Session $session, array $options = array())
+    public function __construct(UserRepositoryInterface $userRepository, Request $request, array $options = array())
     {
         $this->userRepository = $userRepository;
-        $this->session = $session;
+        $this->request = $request;
+        $this->session = $request->getSession();
         $this->options = array_merge(array(
-                    'session_path' => 'doctrine_user/auth/identifier'
-                ));
-
-        // make sure session is started
-        $this->session->start();
+            'session_path' => 'doctrine_user/auth/identifier',
+            'remember_me_cookie_name' => 'doctrine_user/remember_me'
+        ));
     }
 
     /**
      * Log in a user, bind it to the session and update its last login date
      *
+     * @param User $user the user to log in
+     * @param boolean $remember whether or not to remember the user
      * @return null
-     * */
-    public function login(User $user)
+     **/
+    public function login(User $user, $remember = false)
     {
         // bind user identifier to the session
         $this->session->set($this->options['session_path'], $this->getUserIdentifierValue($user));
 
+        if($remember) {
+            // renew user remember_me token
+            $user->renewRememberMeToken();
+            // make token a cookie
+            $this->request->cookies->set($this->options['remember_me_cookie_name'], $user->getRememberMeToken());
+        }
+
         // update user last login date
         $user->setLastLogin(new \DateTime());
+        // save the updated user
         $this->userRepository->getObjectManager()->persist($user);
         $this->userRepository->getObjectManager()->flush();
     }
@@ -68,16 +86,18 @@ class Auth
      * Log out a user
      *
      * @return null
-     * */
+     **/
     public function logout()
     {
-        $this->session->set($this->options['session_path'], null);
+        $this->session->delete($this->options['session_path']);
+        $this->request->cookies->delete($this->options['remember_me_cookie_name']);
     }
 
     /**
      * Get the authenticated user
+     * 
      * @return User
-     * */
+     **/
     public function getUser()
     {
         if (null === $this->user && ($userId = $this->session->get($this->options['session_path']))) {
@@ -91,7 +111,7 @@ class Auth
      * Tell whether or not a user is logged in
      *
      * @return bool
-     * */
+     **/
     public function isAuthenticated()
     {
         return (bool) $this->getUser();
@@ -104,7 +124,7 @@ class Auth
      * @param bool $useAnd
      * 
      * @return bool
-     */
+     **/
     public function hasCredentials($credentials, $useAnd = true)
     {
         if (!$user->isAuthenticated()) {
@@ -144,17 +164,11 @@ class Auth
      * Get the value of the user identifier
      *
      * @return mixed
-     * */
+     **/
     protected function getUserIdentifierValue(User $user)
     {
         $getter = 'get' . ucfirst($this->userRepository->getObjectIdentifier());
 
         return $user->$getter();
     }
-
-    public function getObjectClass()
-    {
-        return $this->options["doctrine_user.auth.class"];
-    }
-
 }
