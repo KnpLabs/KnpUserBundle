8497bb08b18adaa7ab3ae8ac947d47b5cc1706b5
Lukas Kahwe Smith <smith@pooteeweet.org>
2010-10-21 11:38:38 +0200

    added theme support (probably should be injected via the service config), removed some more php tags

--- Form/SessionForm.php
+++ Form/SessionForm.php
@@ -9,10 +9,22 @@ use Symfony\Component\Form\CheckboxField;
 
 class SessionForm extends Form
 {
+    public $theme = array();
+
     public function configure()
     {
         $this->add(new TextField('usernameOrEmail'));
         $this->add(new PasswordField('password'));
         $this->add(new CheckboxField('rememberMe'));
-    } 
+    }
+
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+
+    public function setTheme($theme)
+    {
+        $this->theme = $theme;
+    }
 }
