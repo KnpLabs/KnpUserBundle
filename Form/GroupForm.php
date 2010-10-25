79bd29bec041fdea71a5d1e55c11287fd3703478
Lukas Kahwe Smith <smith@pooteeweet.org>
2010-10-25 16:35:41 +0200

    fixed theme support

--- Form/GroupForm.php
+++ Form/GroupForm.php
@@ -20,7 +20,7 @@ class GroupForm extends Form
      */
     public function __construct($name, $data, ValidatorInterface $validator, array $options = array())
     {
-        $this->addOption('theme', array());
+        $this->addOption('theme');
 
         parent::__construct($name, $data, $validator, $options);
     }
