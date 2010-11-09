3fc2c8336bb85d7a5dc4cf8e3747ce832170858d
ornicar <thibault.duplessis@gmail.com>
2010-11-09 19:00:00 +0100

    Introduce security routing, controller and template

---
+++ Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bundle\DoctrineUserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\SecurityContext;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        // get the error if any (works with forward and redirect -- see below)
+        if ($this['request']->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $this['request']->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $this['request']->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('DoctrineUserBundle:Security:login.twig', array(
+            // last username entered by the user
+            'last_username' => $this['request']->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+}
