b74b9112ef92e36df28a03e34fb8da9791d6ba35
Antoine Hérault <antoine.herault@gmail.com>
2010-12-01 17:07:29 +0100

    Remove Group & Permission system as Symfony2 security is based on roles

--- DoctrineUserBundle.php
+++ DoctrineUserBundle.php
@@ -15,8 +15,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 class DoctrineUserBundle extends BaseBundle
 {
     /**
-     * Get a EntityRepository or a DocumentRepository, based on db driver configuration 
-     * 
+     * Get a EntityRepository or a DocumentRepository, based on db driver configuration
+     *
      * @param mixed $objectManager a EntityManager or a DocumentManager
      * @param mixed $objectClass the class of the entity or document
      * @return mixed a EntityRepository or DocumentRepository
