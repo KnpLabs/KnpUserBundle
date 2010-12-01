beeeaf41eff037919c21a3d8c18323a54e66b7e6
Antoine Hérault <antoine.herault@gmail.com>
2010-12-01 17:22:06 +0100

    Move the getRepository method to the DI extension

--- DoctrineUserBundle.php
+++ DoctrineUserBundle.php
@@ -14,15 +14,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class DoctrineUserBundle extends BaseBundle
 {
-    /**
-     * Get a EntityRepository or a DocumentRepository, based on db driver configuration
-     *
-     * @param mixed $objectManager a EntityManager or a DocumentManager
-     * @param mixed $objectClass the class of the entity or document
-     * @return mixed a EntityRepository or DocumentRepository
-     */
-    public static function getRepository($objectManager, $objectClass)
-    {
-        return $objectManager->getRepository($objectClass);
-    }
 }
