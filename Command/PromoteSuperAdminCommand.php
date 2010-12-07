1a7299313d728394694f583931ed4cc2af489705
ornicar <thibault.duplessis@gmail.com>
2010-12-07 20:23:38 +0100

    Remove unnecessary namespaces in command classes

--- Command/PromoteSuperAdminCommand.php
+++ Command/PromoteSuperAdminCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
-use Bundle\DoctrineUserBundle\Entity\User;
 
 /*
  * This file is part of the DoctrineUserBundle
