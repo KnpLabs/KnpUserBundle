c45176eb95b9521bdd174b55aeacebbb14422784
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-01-04 14:21:13 +0100

    removed outdated license header

--- Validator/Unique.php
+++ Validator/Unique.php
@@ -4,13 +4,6 @@ namespace Bundle\FOS\UserBundle\Validator;
 
 use Symfony\Component\Validator\Constraint;
 
-/**
- * Unique Constraint
- *
- * @author Bulat Shakirzyanov <bulat@theopenskyproject.com>
- * @copyright (c) 2010 OpenSky Project Inc
- * @license http://www.gnu.org/licenses/agpl.txt GNU Affero General Public License
- */
 class Unique extends Constraint
 {
     public $message = 'The value for "%property%" already exists.';
