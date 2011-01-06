feaa9c7dc1d83224f12884ec205000a1585c7fd1
Jimmy Leger <jimmy.leger@gmail.com>
2011-01-06 23:11:52 +0100

    fixed namespaces

--- Command/ActivateUserCommand.php
+++ Command/ActivateUserCommand.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Security\Authentication\Token\UsernamePasswordToken;
+use Bundle\FOS\UserBundle\Model\User;
 
 /*
  * This file is part of the FOS\UserBundle
