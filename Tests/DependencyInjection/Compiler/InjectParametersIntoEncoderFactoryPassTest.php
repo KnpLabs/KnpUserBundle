75026b39879d823c47bf5dd4d31b40daa2c4b9dc
Jeremy Mikola <jmikola@gmail.com>
2011-01-07 13:57:52 -0500

    Revert to using getParameter() in InjectParametersIntoEncoderFactoryPass
    Since this pass happens after ResolveParameterPlaceHoldersPass, do not introduce more "%%" param replacement notation.  This change means that those params can no longer be overridden after Container dumping for this purpose.

--- Tests/DependencyInjection/Compiler/InjectParametersIntoEncoderFactoryPassTest.php
+++ Tests/DependencyInjection/Compiler/InjectParametersIntoEncoderFactoryPassTest.php
@@ -4,6 +4,7 @@ namespace Bundle\FOS\UserBundle\Tests\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Bundle\FOS\UserBundle\DependencyInjection\Compiler\InjectParametersIntoEncoderFactoryPass;
 
@@ -11,22 +12,28 @@ class InjectParametersIntoEncoderFactoryPassTest extends \PHPUnit_Framework_Test
 {
     public function testProcessInjectsEncoderParameters()
     {
-        $container = new ContainerBuilder();
-        $definition = new Definition('class', array('encoderMap'));
+        $injector = new InjectParametersIntoEncoderFactoryPass();
 
+        $container = new ContainerBuilder(new ParameterBag(array(
+            'security.encoder.digest.class'     => 'digest.class',
+            'fos_user.encoder.algorithm'        => 'algorithm',
+            'fos_user.encoder.encode_as_base64' => 'encode_as_base64',
+            'fos_user.encoder.iterations'       => 'iterations',
+        )));
+
+        $definition = new Definition('factory.class', array('factory.encoderMap'));
         $container->setDefinition('security.encoder_factory.generic', $definition);
 
-        $injector = new InjectParametersIntoEncoderFactoryPass();
         $injector->process($container);
 
         $expectedArguments = array(
-            '%security.encoder.digest.class%',
+            'digest.class',
             array(
-                '%fos_user.encoder.algorithm%',
-                '%fos_user.encoder.encode_as_base64%',
-                '%fos_user.encoder.iterations%',
+                'algorithm',
+                'encode_as_base64',
+                'iterations',
             ),
-            'encoderMap',
+            'factory.encoderMap',
         );
 
         $this->assertEquals($expectedArguments, $definition->getArguments());
