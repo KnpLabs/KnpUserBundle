KnpLabs/KnpUserBundle

commit 01537aabf6b949bb820917aae1e40233784c0eb3

fixed password recovery ttl handling

Controller/UserController.php
@@ -239,7 +239,7 @@ class UserController extends ContainerAware
     {
         $user = $this->findUserBy('username', $this->container->get('request')->get('username'));
 
-        if (!$user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
+        if ($user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
             return $this->container->get('templating')->renderResponse('FOSUserBundle:User:passwordAlreadyRequested.html.'.$this->getEngine());
         }
 
@@ -278,7 +278,7 @@ class UserController extends ContainerAware
     {
         $user = $this->findUserBy('confirmationToken', $token);
 
-        if (!$user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
+        if ($user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
             return new RedirectResponse( $this->container->get('router')->generate('fos_user_user_request_reset_password'));
         }
 
@@ -298,7 +298,7 @@ class UserController extends ContainerAware
     {
         $user = $this->findUserBy('confirmationToken', $token);
 
-        if (!$user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
+        if ($user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
             return new RedirectResponse( $this->container->get('router')->generate('fos_user_user_request_reset_password'));
         }
 

commit b7576d7b237646364323befbfc485ae540a17c55

Fixed regression introduced in 01537aab

Controller/UserController.php
@@ -278,7 +278,7 @@ class UserController extends ContainerAware
     {
         $user = $this->findUserBy('confirmationToken', $token);
 
-        if ($user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
+        if (!$user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
             return new RedirectResponse( $this->container->get('router')->generate('fos_user_user_request_reset_password'));
         }
 
@@ -298,7 +298,7 @@ class UserController extends ContainerAware
     {
         $user = $this->findUserBy('confirmationToken', $token);
 
-        if ($user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
+        if (!$user->isPasswordRequestNonExpired($this->getPasswordRequestTtl())) {
             return new RedirectResponse( $this->container->get('router')->generate('fos_user_user_request_reset_password'));
         }
 