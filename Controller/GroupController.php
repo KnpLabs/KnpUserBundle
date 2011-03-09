7c548c8759cfb3603f5cf0c0b0c42bf39a419c2f
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-03-09 13:00:09 +0100

    improved readability

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -101,7 +101,9 @@ class GroupController extends ContainerAware
         $process = $form->process();
         if ($process) {
             $this->container->get('session')->setFlash('fos_user_group_update', 'success');
-            return new RedirectResponse( $this->container->get('router')->generate('fos_user_group_show', array('groupname' => $form->getData('group')->getName())));
+            $parameters = array('groupname' => $form->getData('group')->getName());
+            $url = $this->container->get('router')->generate('fos_user_group_show', $parameters);
+            return new RedirectResponse($url);
         }
 
         return $this->container->get('templating')->renderResponse('FOSUserBundle:Group:new.html.'.$this->getEngine(), array(
