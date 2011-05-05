f6d4b17d1b6b0469b8e86e413eb9452039e75f9a
Christophe Coevoet <stof@notk.org>
2011-05-05 18:02:21 +0200

    Fixed autoloading in tests as Doctrine vendors are also needed

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,18 +1,7 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'FOS\\UserBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        require_once __DIR__.'/../'.$path;
-        return true;
-    }
-});
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
