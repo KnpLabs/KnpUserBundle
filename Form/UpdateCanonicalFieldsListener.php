46733473ef40ff59470de4fcbef0a1a729bafdc5
Christophe Coevoet <stof@notk.org>
2011-05-09 02:14:27 +0200

    Added a postBind listener to update the canonical fields. closes #124

---
+++ Form/UpdateCanonicalFieldsListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace FOS\UserBundle\Form;
+
+use Symfony\Component\Form\Events;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\Form\Event\DataEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use FOS\UserBundle\Model\UserInterface;
+
+/**
+ * Listener to update the canonical fields when binding the form
+ */
+class UpdateCanonicalFieldsListener implements EventSubscriberInterface
+{
+    /**
+     * @var UserManagerInterface
+     */
+    private $userManager;
+
+    public function __construct(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return Events::postBind;
+    }
+
+    public function postBind(DataEvent $event)
+    {
+        $user = $event->getForm()->getData();
+        if ($user instanceof UserInterface) {
+            $this->userManager->updateCanonicalFields($user);
+        }
+    }
+}
