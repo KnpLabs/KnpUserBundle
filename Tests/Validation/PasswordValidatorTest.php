887e402eaf45d069f8601b31051cce340148acaa
Christophe Coevoet <stof@notk.org>
2011-05-12 21:09:54 +0200

    Cleaned Validator tests

--- Tests/Validation/PasswordValidatorTest.php
+++ Tests/Validation/PasswordValidatorTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Symfony FOSUB
+ * This file is part of the FOS UserBundle
  *
  * (c) Luis Cordova <cordoval@gmail.com>
  *
@@ -9,42 +9,32 @@
  * file that was distributed with this source code.
  */
 
-
 namespace FOS\UserBundle\Tests\Validation;
 
-use FOS\UserBundle\Model\UserManagerInterface;
 use FOS\UserBundle\Validator\PasswordValidator;
 use FOS\UserBundle\Validator\Password;
-use FOS\UserBundle\Tests\TestUser;
 use FOS\UserBundle\Form\ChangePassword;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
-use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
 class PasswordValidatorTest extends \PHPUnit_Framework_TestCase
 {
-    protected $validator;
-    protected $userManagerMock;
-    protected $constraint;
-    protected $encoderFactory;
-    protected $userObject;
-    protected $changePasswordObject;
-    protected $nonObjectProperty;
-    protected $encoder;
+    private $validator;
+    private $constraint;
+    private $encoderFactory;
+    private $changePasswordObject;
+    private $encoder;
 
     public function setUp()
     {
-        $this->userManagerMock = $this->createUserManagerMock(array('validateUnique'));
-        $this->constraintMock = new Password();//$this->createPasswordConstraintMock(array());
-        $this->constraintMock->passwordProperty = 'current';
+        $this->constraint = new Password();
+        $this->constraint->passwordProperty = 'current';
+        $this->constraint->userProperty = 'user';
+
+        $this->changePasswordObject = new ChangePassword($this->getMock('FOS\UserBundle\Model\UserInterface'));
+        $this->encoderFactory = $this->getMock('Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface');
+        $this->encoder = $this->getMock('Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface');
+
         $this->validator = new PasswordValidator();
-        $this->encoderFactory = $this->createEncoderFactoryMock(array('getEncoder'));
-        $this->encoder = $this->createEncoderMock(array());
         $this->validator->setEncoderFactory($this->encoderFactory);
-        $this->userObject = $this->createUserMock(array('getPassword', 'getSalt'));
-        $this->constraintMock->userProperty = 'user'; //$this->userObject;
-        $this->nonObjectProperty = 'propertyValue';
-        $this->changePasswordObject = $this->createchangePasswordObjectMock(array());
-        $this->changePasswordObject->user = $this->userObject;
     }
 
     /**
@@ -52,7 +42,7 @@ class PasswordValidatorTest extends \PHPUnit_Framework_TestCase
      */
     public function testExceptionForNonObject()
     {
-        $this->validator->isValid($this->nonObjectProperty, $this->constraintMock);
+        $this->validator->isValid('propertyValue', $this->constraint);
     }
 
     public function testFalseOnInvalidPassword()
@@ -65,8 +55,7 @@ class PasswordValidatorTest extends \PHPUnit_Framework_TestCase
                 ->method('isPasswordValid')
                 ->will($this->returnValue(false));
 
-        $this->validator->setEncoderFactory($this->encoderFactory);
-        $this->assertFalse($this->validator->isValid($this->changePasswordObject, $this->constraintMock));
+        $this->assertFalse($this->validator->isValid($this->changePasswordObject, $this->constraint));
     }
 
     public function testTrueOnValidPassword()
@@ -79,61 +68,6 @@ class PasswordValidatorTest extends \PHPUnit_Framework_TestCase
                 ->method('isPasswordValid')
                 ->will($this->returnValue(true));
 
-        $this->validator->setEncoderFactory($this->encoderFactory);
-        $this->assertTrue($this->validator->isValid($this->changePasswordObject, $this->constraintMock));
-    }
-
-    protected function createUserManagerMock(array $methods)
-    {
-        $userManager = $this->getMockBuilder('FOS\UserBundle\Model\UserManagerInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        return $userManager;
-    }
-
-    protected function createPasswordConstraintMock(array $methods)
-    {
-        $constraint = $this->getMockBuilder('FOS\UserBundle\Validator\Password')
-            //->disableOriginalConstructor()
-            ->getMock();
-        
-        return $constraint;
-    }
-
-    protected function createUserMock(array $methods)
-    {
-        $constraint = $this->getMockBuilder('FOS\UserBundle\Model\User')
-            ->disableOriginalConstructor()
-            ->getMockForAbstractClass();
-
-        return $constraint;
-    }
-
-    protected function createEncoderFactoryMock(array $methods)
-    {
-        $encoderFactory = $this->getMockBuilder('Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface')
-           ->disableOriginalConstructor()
-           ->getMock();
-        
-        return $encoderFactory;
-    }
-
-    protected function createchangePasswordObjectMock(array $methods)
-    {
-        $changePasswordObject = $this->getMockBuilder('FOS\UserBundle\Form\ChangePassword')
-           ->disableOriginalConstructor()
-           ->getMock();
-
-        return $changePasswordObject;
-    }
-
-    protected function createEncoderMock(array $methods)
-    {
-        $encoder = $this->getMockBuilder('Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface')
-           ->disableOriginalConstructor()
-           ->getMock();
-
-        return $encoder;
+        $this->assertTrue($this->validator->isValid($this->changePasswordObject, $this->constraint));
     }
 }
--- Tests/Validation/UniqueValidatorTest.php
+++ Tests/Validation/UniqueValidatorTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Symfony FOSUB
+ * This file is part of the FOS UserBundle
  *
  * (c) Luis Cordova <cordoval@gmail.com>
  *
@@ -9,26 +9,22 @@
  * file that was distributed with this source code.
  */
 
-
 namespace FOS\UserBundle\Tests\Validation;
 
-use FOS\UserBundle\Model\UserManagerInterface;
 use FOS\UserBundle\Validator\UniqueValidator;
 use FOS\UserBundle\Validator\Unique;
 
 class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
 {
-    protected $validator;
-    protected $userManagerMock;
-    protected $constraint;
-    protected $userProperty;
+    private $validator;
+    private $userManagerMock;
+    private $constraint;
 
     public function setUp()
     {
-        $this->userManagerMock = $this->createUserManagerMock(array('validateUnique'));
-        $this->constraintMock = $this->createUniqueConstraintMock(array());
+        $this->userManagerMock = $this->getMock('FOS\UserBundle\Model\UserManagerInterface');
+        $this->constraint = new Unique();
         $this->validator = new UniqueValidator($this->userManagerMock);
-        $this->userProperty = 'propertyValue';
     }
 
     public function testFalseOnDuplicateUserProperty()
@@ -36,9 +32,9 @@ class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
         $this->userManagerMock->expects($this->once())
                 ->method('validateUnique')
                 ->will($this->returnValue(false))
-                ->with($this->equalTo($this->userProperty), $this->equalTo($this->constraintMock));
+                ->with($this->equalTo('propertyValue'), $this->equalTo($this->constraint));
 
-        $this->assertFalse($this->validator->isValid($this->userProperty, $this->constraintMock));
+        $this->assertFalse($this->validator->isValid('propertyValue', $this->constraint));
     }
 
     public function testTrueOnUniqueUserProperty()
@@ -46,26 +42,8 @@ class UniqueValidatorTest extends \PHPUnit_Framework_TestCase
         $this->userManagerMock->expects($this->once())
                 ->method('validateUnique')
                 ->will($this->returnValue(true))
-                ->with($this->equalTo($this->userProperty), $this->equalTo($this->constraintMock));
+                ->with($this->equalTo('propertyValue'), $this->equalTo($this->constraint));
 
-        $this->assertTrue($this->validator->isValid($this->userProperty, $this->constraintMock));
-    }
-
-    protected function createUserManagerMock(array $methods)
-    {
-        $userManager = $this->getMockBuilder('FOS\UserBundle\Model\UserManagerInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        return $userManager;
-    }
-
-    protected function createUniqueConstraintMock(array $methods)
-    {
-        $constraint = $this->getMockBuilder('FOS\UserBundle\Validator\Unique')
-            ->disableOriginalConstructor()
-            ->getMock();
-        
-        return $constraint;
+        $this->assertTrue($this->validator->isValid('propertyValue', $this->constraint));
     }
 }
