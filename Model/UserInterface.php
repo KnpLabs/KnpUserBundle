7a392bb8efc28bccca8fc6adf1fa775a9c62e946
Christophe Coevoet <stof@notk.org>
2011-06-19 22:10:28 +0200

    Made the UserInterface extend Serializable

--- Model/UserInterface.php
+++ Model/UserInterface.php
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Core\User\AdvancedUserInterface;
  * @author Thibault Duplessis <thibault.duplessis@gmail.com>
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  */
-interface UserInterface extends AdvancedUserInterface
+interface UserInterface extends AdvancedUserInterface, \Serializable
 {
     function addRole($role);
 
