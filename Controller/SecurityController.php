cebc1bef837db5df5d1319badb89e6ef14b92d7c
Christophe Coevoet <stof@notk.org>
2011-07-08 19:33:45 +0200

    Fixed the SecurityController when there is no session
    Closes #212

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -19,22 +19,30 @@ class SecurityController extends ContainerAware
 {
     public function loginAction()
     {
+        $request = $this->container->get('request');
+        /* @var $request \Symfony\Component\HttpFoundation\Request */
+        $session = $request->getSession();
+        /* @var $session \Symfony\Component\HttpFoundation\Session */
+
         // get the error if any (works with forward and redirect -- see below)
-        if ($this->container->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $this->container->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
         } else {
-            $error = $this->container->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
-            $this->container->get('request')->getSession()->remove(SecurityContext::AUTHENTICATION_ERROR);
+            $error = '';
         }
 
         if ($error) {
             // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
             $error = $error->getMessage();
         }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
 
         return $this->container->get('templating')->renderResponse('FOSUserBundle:Security:login.html.'.$this->container->getParameter('fos_user.template.engine'), array(
-            // last username entered by the user
-            'last_username' => $this->container->get('request')->getSession()->get(SecurityContext::LAST_USERNAME),
+            'last_username' => $lastUsername,
             'error'         => $error,
         ));
     }
