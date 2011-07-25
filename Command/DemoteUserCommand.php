9d58830a2d9705000384004a7841744e5f164755
lenar <lenar@city.ee>
2011-07-25 22:03:59 +0300

    Refactor promote/demote commands to really use RoleCommand

--- Command/DemoteUserCommand.php
+++ Command/DemoteUserCommand.php
@@ -11,33 +11,25 @@
 
 namespace FOS\UserBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use FOS\UserBundle\Model\User;
+use FOS\UserBundle\Util\UserManipulator;
 
 /**
  * @author Antoine Hérault <antoine.herault@gmail.com>
  * @author Lenar Lõhmus <lenar@city.ee>
  */
-class DemoteUserCommand extends ContainerAwareCommand
+class DemoteUserCommand extends RoleCommand
 {
     /**
      * @see Command
      */
     protected function configure()
     {
+        parent::configure();
+        
         $this
             ->setName('fos:user:demote')
             ->setDescription('Demote a user by removing a role')
-            ->setDefinition(array(
-                new InputArgument('username', InputArgument::REQUIRED, 'The username'),
-                new InputArgument('role', InputArgument::OPTIONAL, 'The role'),
-                new InputOption('super', null, InputOption::VALUE_NONE, 'Instead specifying role, use this to quickly remove the super administrator role'),
-            ))
             ->setHelp(<<<EOT
 The <info>fos:user:demote</info> command demotes a user by removing a role
 
@@ -46,25 +38,9 @@ The <info>fos:user:demote</info> command demotes a user by removing a role
 EOT
             );
     }
-
-    /**
-     * @see Command
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    
+    protected function executeRoleCommand(UserManipulator $manipulator, OutputInterface $output, $username, $super, $role)
     {
-        $username = $input->getArgument('username');
-        $role = $input->getArgument('role');
-        $super = (true === $input->getOption('super'));
-
-        if (null !== $role && $super) {
-            throw new \InvalidArgumentException('You can pass either the role or the --super option (but not both simultaneously).');
-        }
-
-        if (null === $role && !$super) {
-            throw new \RuntimeException('Not enough arguments.');
-        }
-
-        $manipulator = $this->getContainer()->get('fos_user.util.user_manipulator');
         if ($super) {
             $manipulator->demote($username);
             $output->writeln(sprintf('User "%s" has been demoted as a simple user.', $username));
@@ -74,41 +50,6 @@ EOT
             } else {
                 $output->writeln(sprintf('User "%s" didn\'t have "%s" role.', $username, $role));
             }
-        }
-    }
-
-    /**
-     * @see Command
-     */
-    protected function interact(InputInterface $input, OutputInterface $output)
-    {
-        if (!$input->getArgument('username')) {
-            $username = $this->getHelper('dialog')->askAndValidate(
-                $output,
-                'Please choose a username:',
-                function($username)
-                {
-                    if (empty($username)) {
-                        throw new \Exception('Username can not be empty');
-                    }
-                    return $username;
-                }
-            );
-            $input->setArgument('username', $username);
-        }
-        if ((true !== $input->getOption('super')) && !$input->getArgument('role')) {
-            $role = $this->getHelper('dialog')->askAndValidate(
-                $output,
-                'Please choose a role:',
-                function($role)
-                {
-                    if (empty($role)) {
-                        throw new \Exception('Role can not be empty');
-                    }
-                    return $role;
-                }
-            );
-            $input->setArgument('role', $role);
         }
     }
 }
--- Command/PromoteUserCommand.php
+++ Command/PromoteUserCommand.php
@@ -11,13 +11,8 @@
 
 namespace FOS\UserBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use FOS\UserBundle\Model\User;
+use FOS\UserBundle\Util\UserManipulator;
 
 /**
  * @author Matthieu Bontemps <matthieu@knplabs.com>
@@ -25,21 +20,18 @@ use FOS\UserBundle\Model\User;
  * @author Luis Cordova <cordoval@gmail.com>
  * @author Lenar Lõhmus <lenar@city.ee>
  */
-class PromoteUserCommand extends ContainerAwareCommand
+class PromoteUserCommand extends RoleCommand
 {
     /**
      * @see Command
      */
     protected function configure()
     {
+        parent::configure();
+
         $this
             ->setName('fos:user:promote')
             ->setDescription('Promotes a user by adding a role')
-            ->setDefinition(array(
-                new InputArgument('username', InputArgument::REQUIRED, 'The username'),
-                new InputArgument('role', InputArgument::OPTIONAL, 'The role'),
-                new InputOption('super', null, InputOption::VALUE_NONE, 'Instead specifying role, use this to quickly add the super administrator role'),
-            ))
             ->setHelp(<<<EOT
 The <info>fos:user:promote</info> command promotes a user by adding a role
 
@@ -49,24 +41,8 @@ EOT
             );
     }
 
-    /**
-     * @see Command
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function executeRoleCommand(UserManipulator $manipulator, OutputInterface $output, $username, $super, $role)
     {
-        $username = $input->getArgument('username');
-        $role = $input->getArgument('role');
-        $super = (true === $input->getOption('super'));
-
-        if (null !== $role && $super) {
-            throw new \InvalidArgumentException('You can pass either the role or the --super option (but not both simultaneously).');
-        }
-
-        if (null === $role && !$super) {
-            throw new \RuntimeException('Not enough arguments.');
-        }
-
-        $manipulator = $this->getContainer()->get('fos_user.util.user_manipulator');
         if ($super) {
             $manipulator->promote($username);
             $output->writeln(sprintf('User "%s" has been promoted as a super administrator.', $username));
@@ -76,41 +52,6 @@ EOT
             } else {
                 $output->writeln(sprintf('User "%s" did already have "%s" role.', $username, $role));
             }
-        }
-    }
-
-    /**
-     * @see Command
-     */
-    protected function interact(InputInterface $input, OutputInterface $output)
-    {
-        if (!$input->getArgument('username')) {
-            $username = $this->getHelper('dialog')->askAndValidate(
-                $output,
-                'Please choose a username:',
-                function($username)
-                {
-                    if (empty($username)) {
-                        throw new \Exception('Username can not be empty');
-                    }
-                    return $username;
-                }
-            );
-            $input->setArgument('username', $username);
-        }
-        if ((true !== $input->getOption('super')) && !$input->getArgument('role')) {
-            $role = $this->getHelper('dialog')->askAndValidate(
-                $output,
-                'Please choose a role:',
-                function($role)
-                {
-                    if (empty($role)) {
-                        throw new \Exception('Role can not be empty');
-                    }
-                    return $role;
-                }
-            );
-            $input->setArgument('role', $role);
         }
     }
 }
