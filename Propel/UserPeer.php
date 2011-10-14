b70b3765e1d6287e3f80e30a84f1dc8b9f589a78
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-10-14 21:25:36 +0200

    Init propel schema

---
+++ Propel/UserPeer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FOS\UserBundle\Propel;
+
+use FOS\UserBundle\Propel\om\BaseUserPeer;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'user' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.host/mach/vendor/bundles/FOS/UserBundle/Propel
+ */
+class UserPeer extends BaseUserPeer {
+
+} // UserPeer
