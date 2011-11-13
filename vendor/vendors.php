3f0a20a55555cd0ae1e4e7f865674ba04998db60
Christophe Coevoet <stof@notk.org>
2011-11-13 16:26:44 +0100

    Added the quiet option for the clone

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -30,7 +30,7 @@ foreach ($deps as $dep) {
 
     $installDir = $vendorDir.'/'.$name;
     if (!is_dir($installDir)) {
-        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+        system(sprintf('git clone -q %s %s', escapeshellarg($url), escapeshellarg($installDir)));
     }
 
     system(sprintf('cd %s && git fetch -q origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
