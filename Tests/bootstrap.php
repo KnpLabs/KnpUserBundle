e947283841b3c90d90be1ec5930942ab53881b85
Johannes M. Schmitt <schmittjoh@gmail.com>
2012-07-10 12:32:08 +0200

    updated test suite to use composer

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,11 +9,23 @@
  * file that was distributed with this source code.
  */
 
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
+
+require $autoloadFile;
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'FOS\\UserBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+
+        return true;
+    }
+});
 
 if (class_exists('PropelQuickBuilder') && class_exists('TypehintableBehavior')) {
     $class = new \ReflectionClass('TypehintableBehavior');
