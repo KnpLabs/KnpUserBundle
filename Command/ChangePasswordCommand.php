FriendsOfSymfony/FOSUserBundle

Hide user password in CLI if possible

Command/ChangePasswordCommand.php
@@ -83,7 +83,8 @@ EOT
         }
 
         if (!$input->getArgument('password')) {
-            $password = $this->getHelper('dialog')->askAndValidate(
+            $asking_method = method_exists($this->getHelper('dialog'), 'askHiddenResponseAndValidate') ? 'askHiddenResponseAndValidate' : 'askAndValidate';
+            $password = $this->getHelper('dialog')->$asking_method(
                 $output,
                 'Please enter the new password:',
                 function($password) {

Command/CreateUserCommand.php
@@ -115,7 +115,8 @@ EOT
         }
 
         if (!$input->getArgument('password')) {
-            $password = $this->getHelper('dialog')->askAndValidate(
+            $asking_method = method_exists($this->getHelper('dialog'), 'askHiddenResponseAndValidate') ? 'askHiddenResponseAndValidate' : 'askAndValidate';
+            $password = $this->getHelper('dialog')->$asking_method(
                 $output,
                 'Please choose a password:',
                 function($password) {

KnpLabs/KnpUserBundle

Command/ChangePasswordCommand.php
@@ -83,7 +83,8 @@ EOT
         }
 
         if (!$input->getArgument('password')) {
-            $password = $this->getHelper('dialog')->askAndValidate(
+            $askingMethod = method_exists($this->getHelper('dialog'), 'askHiddenResponseAndValidate') ? 'askHiddenResponseAndValidate' : 'askAndValidate';
+            $password = $this->getHelper('dialog')->$askingMethod(
                 $output,
                 'Please enter the new password:',
                 function($password) {

Command/CreateUserCommand.php
@@ -115,7 +115,8 @@ EOT
         }
 
         if (!$input->getArgument('password')) {
-            $password = $this->getHelper('dialog')->askAndValidate(
+            $askingMethod = method_exists($this->getHelper('dialog'), 'askHiddenResponseAndValidate') ? 'askHiddenResponseAndValidate' : 'askAndValidate';
+            $password = $this->getHelper('dialog')->$askingMethod(
                 $output,
                 'Please choose a password:',
                 function($password) {