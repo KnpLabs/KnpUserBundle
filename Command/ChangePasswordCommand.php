131cfda852d750ffb6f9c9278baeed814eb9b25b
Gábor Egyed <gabor.egyed@gmail.com>
2015-11-17 23:47:01 +0100

    Update change password command

--- Command/ChangePasswordCommand.php
+++ Command/ChangePasswordCommand.php
@@ -15,6 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 /**
  * ChangePasswordCommand
@@ -66,6 +67,46 @@ EOT
      * @see Command
      */
     protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if (!class_exists('Symfony\Component\Console\Helper\QuestionHelper')) {
+            return $this->legacyInteract($input, $output);
+        }
+
+        $questions = array();
+
+        if (!$input->getArgument('username')) {
+            $question = new Question('Please give the username:');
+            $question->setValidator(function($username) {
+                if (empty($username)) {
+                    throw new \Exception('Username can not be empty');
+                }
+
+                return $username;
+            });
+            $questions['username'] = $question;
+        }
+
+        if (!$input->getArgument('password')) {
+            $question = new Question('Please enter the new password:');
+            $question->setValidator(function($password) {
+                if (empty($password)) {
+                    throw new \Exception('Password can not be empty');
+                }
+
+                return $password;
+            });
+            $question->setHidden(true);
+            $questions['password'] = $question;
+        }
+
+        foreach ($questions as $name => $question) {
+            $answer = $this->getHelper('question')->ask($input, $output, $question);
+            $input->setArgument($name, $answer);
+        }
+    }
+
+    // BC for SF <2.5
+    protected function legacyInteract(InputInterface $input, OutputInterface $output)
     {
         if (!$input->getArgument('username')) {
             $username = $this->getHelper('dialog')->askAndValidate(
---
+++ Tests/Command/ChangePasswordCommandTest.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\UserBundle\Tests\Command;
+
+use FOS\UserBundle\Command\ChangePasswordCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ChangePasswordCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        $commandTester = $this->createCommandTester($this->getContainer('user', 'pass'));
+        $exitCode = $commandTester->execute(array(
+            'command' => 'fos:user:change-password', // BC for SF <2.4 see https://github.com/symfony/symfony/pull/8626
+            'username' => 'user',
+            'password' => 'pass',
+        ), array(
+            'decorated' => false,
+            'interactive' => false,
+        ));
+
+        $this->assertEquals(0, $exitCode, 'Returns 0 in case of success');
+        $this->assertRegExp('/Changed password for user user/', $commandTester->getDisplay());
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testExecuteInteractiveWithDialogHelper()
+    {
+        if (class_exists('Symfony\Component\Console\Helper\QuestionHelper')) {
+            $this->markTestSkipped('Legacy test. The question helper used instead.');
+        }
+
+        $application = new Application();
+
+        $dialog = $this->getMock('Symfony\Component\Console\Helper\DialogHelper', array(
+            'askAndValidate',
+            'askHiddenResponseAndValidate',
+        ));
+        $dialog->expects($this->at(0))
+            ->method('askAndValidate')
+            ->will($this->returnValue('user'));
+        $dialog->expects($this->at(1))
+            ->method('askHiddenResponseAndValidate')
+            ->will($this->returnValue('pass'));
+
+        $application->getHelperSet()->set($dialog, 'dialog');
+
+        $commandTester = $this->createCommandTester($this->getContainer('user', 'pass'), $application);
+        $exitCode = $commandTester->execute(array(
+            'command' => 'fos:user:change-password', // BC for SF <2.4 see https://github.com/symfony/symfony/pull/8626
+        ), array(
+            'decorated' => false,
+            'interactive' => true,
+        ));
+
+        $this->assertEquals(0, $exitCode, 'Returns 0 in case of success');
+        $this->assertRegExp('/Changed password for user user/', $commandTester->getDisplay());
+    }
+
+    public function testExecuteInteractiveWithQuestionHelper()
+    {
+        if (!class_exists('Symfony\Component\Console\Helper\QuestionHelper')) {
+            $this->markTestSkipped('The question helper not available.');
+        }
+
+        $application = new Application();
+
+        $helper = $this->getMock('Symfony\Component\Console\Helper\QuestionHelper', array(
+            'ask',
+        ));
+        $helper->expects($this->at(0))
+            ->method('ask')
+            ->will($this->returnValue('user'));
+        $helper->expects($this->at(1))
+            ->method('ask')
+            ->will($this->returnValue('pass'));
+
+        $application->getHelperSet()->set($helper, 'question');
+
+        $commandTester = $this->createCommandTester($this->getContainer('user', 'pass'), $application);
+        $exitCode = $commandTester->execute(array(), array(
+            'decorated' => false,
+            'interactive' => true,
+        ));
+
+        $this->assertEquals(0, $exitCode, 'Returns 0 in case of success');
+        $this->assertRegExp('/Changed password for user user/', $commandTester->getDisplay());
+    }
+
+    private function createCommandTester(ContainerInterface $container, Application $application = null)
+    {
+        if (null === $application) {
+            $application = new Application();
+        }
+
+        $application->setAutoExit(false);
+
+        $command = new ChangePasswordCommand();
+        $command->setContainer($container);
+
+        $application->add($command);
+
+        return new CommandTester($application->find('fos:user:change-password'));
+    }
+
+    private function getContainer($username, $password)
+    {
+        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+
+        $manipulator = $this->getMockBuilder('FOS\UserBundle\Util\UserManipulator')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $manipulator
+            ->expects($this->once())
+            ->method('changePassword')
+            ->with($username, $password)
+        ;
+
+        $container
+            ->expects($this->once())
+            ->method('get')
+            ->with('fos_user.util.user_manipulator')
+            ->will($this->returnValue($manipulator));
+
+        return $container;
+    }
+}
