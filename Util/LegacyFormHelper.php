63dff9260e0b64df32ef4c1e1dbd84bc61cf3abe
Gábor Egyed <gabor.egyed@gmail.com>
2015-11-21 20:26:49 +0100

    Add missing license header

--- Util/LegacyFormHelper.php
+++ Util/LegacyFormHelper.php
@@ -1,9 +1,20 @@
 <?php
 
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FOS\UserBundle\Util;
 
 /**
  * @internal
+ *
+ * @author Gabor Egyed <gabor.egyed@gmail.com>
  */
 final class LegacyFormHelper
 {
