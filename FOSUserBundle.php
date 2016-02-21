08f6481c794f1d1cfb9d6eedb57b8ba08223ed08
David Buchmann <david.buchmann@liip.ch>
2016-02-21 22:28:48 +0100

    remove code that was only needed for symfony 2.2 and older

--- FOSUserBundle.php
+++ FOSUserBundle.php
@@ -38,29 +38,20 @@ class FOSUserBundle extends Bundle
      */
     private function addRegisterMappingsPass(ContainerBuilder $container)
     {
-        // the base class is only available since symfony 2.3
-        $symfonyVersion = class_exists('Symfony\Bridge\Doctrine\DependencyInjection\CompilerPass\RegisterMappingsPass');
-
         $mappings = array(
             realpath(__DIR__ . '/Resources/config/doctrine-mapping') => 'FOS\UserBundle\Model',
         );
 
-        if ($symfonyVersion && class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
+        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
             $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings, array('fos_user.model_manager_name'), 'fos_user.backend_type_orm'));
-        } else {
-            $container->addCompilerPass(RegisterMappingsPass::createOrmMappingDriver($mappings));
         }
 
-        if ($symfonyVersion && class_exists('Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass')) {
+        if (class_exists('Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass')) {
             $container->addCompilerPass(DoctrineMongoDBMappingsPass::createXmlMappingDriver($mappings, array('fos_user.model_manager_name'), 'fos_user.backend_type_mongodb'));
-        } else {
-            $container->addCompilerPass(RegisterMappingsPass::createMongoDBMappingDriver($mappings));
         }
 
-        if ($symfonyVersion && class_exists('Doctrine\Bundle\CouchDBBundle\DependencyInjection\Compiler\DoctrineCouchDBMappingsPass')) {
+        if (class_exists('Doctrine\Bundle\CouchDBBundle\DependencyInjection\Compiler\DoctrineCouchDBMappingsPass')) {
             $container->addCompilerPass(DoctrineCouchDBMappingsPass::createXmlMappingDriver($mappings, array('fos_user.model_manager_name'), 'fos_user.backend_type_couchdb'));
-        } else {
-            $container->addCompilerPass(RegisterMappingsPass::createCouchDBMappingDriver($mappings));
         }
     }
 }
