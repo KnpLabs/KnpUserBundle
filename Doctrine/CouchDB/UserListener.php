williamjehanne/FOSUserBundle

Refactored the update of the hashed password and canonical fields
The responsibility of updating these fields is moved to dedicated
services instead of being in the UserManager, allowing to inject them in
the Doctrine listener and the validator initializer without any issue
about circular dependencies.

Doctrine/CouchDB/UserListener.php
@@ -15,28 +15,18 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\ODM\CouchDB\Event;
 use Doctrine\ODM\CouchDB\Event\LifecycleEventArgs;
 use FOS\UserBundle\Model\UserInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+use FOS\UserBundle\Util\PasswordUpdater;
 
 class UserListener implements EventSubscriber
 {
-    /**
-     * @var \FOS\UserBundle\Model\UserManagerInterface
-     */
-    private $userManager;
+    private $passwordUpdater;
+    private $canonicalFieldsUpdater;
 
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /**
-     * Constructor
-     *
-     * @param ContainerInterface $container
-     */
-    public function __construct(ContainerInterface $container)
+    public function __construct(PasswordUpdater $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater)
     {
-        $this->container = $container;
+        $this->passwordUpdater = $passwordUpdater;
+        $this->canonicalFieldsUpdater = $canonicalFieldsUpdater;
     }
 
     public function getSubscribedEvents()
@@ -76,11 +66,7 @@ class UserListener implements EventSubscriber
      */
     private function updateUserFields(UserInterface $user)
     {
-        if (null === $this->userManager) {
-            $this->userManager = $this->container->get('fos_user.user_manager');
-        }
-
-        $this->userManager->updateCanonicalFields($user);
-        $this->userManager->updatePassword($user);
+        $this->canonicalFieldsUpdater->updateCanonicalFields($user);
+        $this->passwordUpdater->hashPassword($user);
     }
 }

Doctrine/UserListener.php
@@ -17,7 +17,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ORM\EntityManager;
 use FOS\UserBundle\Model\UserInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+use FOS\UserBundle\Util\PasswordUpdater;
 
 /**
  * Doctrine listener updating the canonical username and password fields.
@@ -27,24 +28,13 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class UserListener implements EventSubscriber
 {
-    /**
-     * @var \FOS\UserBundle\Model\UserManagerInterface
-     */
-    private $userManager;
+    private $passwordUpdater;
+    private $canonicalFieldsUpdater;
 
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /**
-     * Constructor
-     *
-     * @param ContainerInterface $container
-     */
-    public function __construct(ContainerInterface $container)
+    public function __construct(PasswordUpdater $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater)
     {
-        $this->container = $container;
+        $this->passwordUpdater = $passwordUpdater;
+        $this->canonicalFieldsUpdater = $canonicalFieldsUpdater;
     }
 
     public function getSubscribedEvents()
@@ -89,12 +79,8 @@ class UserListener implements EventSubscriber
      */
     private function updateUserFields(UserInterface $user)
     {
-        if (null === $this->userManager) {
-            $this->userManager = $this->container->get('fos_user.user_manager');
-        }
-
-        $this->userManager->updateCanonicalFields($user);
-        $this->userManager->updatePassword($user);
+        $this->canonicalFieldsUpdater->updateCanonicalFields($user);
+        $this->passwordUpdater->hashPassword($user);
     }
 
     /**

Tests/Util/CanonicalFieldsUpdaterTest.php
@@ -1,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\UserBundle\Tests\Util;
+
+use FOS\UserBundle\Tests\TestUser;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+
+class CanonicalFieldsUpdaterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CanonicalFieldsUpdater
+     */
+    private $updater;
+    private $usernameCanonicalizer;
+    private $emailCanonicalizer;
+
+    protected function setUp()
+    {
+        $this->usernameCanonicalizer = $this->getMockCanonicalizer();
+        $this->emailCanonicalizer = $this->getMockCanonicalizer();
+
+        $this->updater = new CanonicalFieldsUpdater($this->usernameCanonicalizer, $this->emailCanonicalizer);
+    }
+
+    public function testUpdateCanonicalFields()
+    {
+        $user = new TestUser();
+        $user->setUsername('Username');
+        $user->setEmail('User@Example.com');
+
+        $this->usernameCanonicalizer->expects($this->once())
+            ->method('canonicalize')
+            ->with('Username')
+            ->will($this->returnCallback('strtolower'));
+
+        $this->emailCanonicalizer->expects($this->once())
+            ->method('canonicalize')
+            ->with('User@Example.com')
+            ->will($this->returnCallback('strtolower'));
+
+        $this->updater->updateCanonicalFields($user);
+        $this->assertEquals('username', $user->getUsernameCanonical());
+        $this->assertEquals('user@example.com', $user->getEmailCanonical());
+    }
+
+    private function getMockCanonicalizer()
+    {
+        return $this->getMock('FOS\UserBundle\Util\CanonicalizerInterface');
+    }
+}

KnpLabs/KnpUserBundle

Doctrine/CouchDB/UserListener.php
@@ -15,29 +15,18 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\ODM\CouchDB\Event;
 use Doctrine\ODM\CouchDB\Event\LifecycleEventArgs;
 use FOS\UserBundle\Model\UserInterface;
-use FOS\UserBundle\Model\UserManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+use FOS\UserBundle\Util\PasswordUpdaterInterface;
 
 class UserListener implements EventSubscriber
 {
-    /**
-     * @var UserManagerInterface
-     */
-    private $userManager;
+    private $passwordUpdater;
+    private $canonicalFieldsUpdater;
 
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /**
-     * Constructor.
-     *
-     * @param ContainerInterface $container
-     */
-    public function __construct(ContainerInterface $container)
+    public function __construct(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater)
     {
-        $this->container = $container;
+        $this->passwordUpdater = $passwordUpdater;
+        $this->canonicalFieldsUpdater = $canonicalFieldsUpdater;
     }
 
     /**
@@ -80,11 +69,7 @@ class UserListener implements EventSubscriber
      */
     private function updateUserFields(UserInterface $user)
     {
-        if (null === $this->userManager) {
-            $this->userManager = $this->container->get('fos_user.user_manager');
-        }
-
-        $this->userManager->updateCanonicalFields($user);
-        $this->userManager->updatePassword($user);
+        $this->canonicalFieldsUpdater->updateCanonicalFields($user);
+        $this->passwordUpdater->hashPassword($user);
     }
 }

Doctrine/UserListener.php
@@ -17,8 +17,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ORM\EntityManager;
 use FOS\UserBundle\Model\UserInterface;
-use FOS\UserBundle\Model\UserManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+use FOS\UserBundle\Util\PasswordUpdaterInterface;
 
 /**
  * Doctrine listener updating the canonical username and password fields.
@@ -28,24 +28,13 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class UserListener implements EventSubscriber
 {
-    /**
-     * @var UserManagerInterface
-     */
-    private $userManager;
+    private $passwordUpdater;
+    private $canonicalFieldsUpdater;
 
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /**
-     * Constructor.
-     *
-     * @param ContainerInterface $container
-     */
-    public function __construct(ContainerInterface $container)
+    public function __construct(PasswordUpdaterInterface $passwordUpdater, CanonicalFieldsUpdater $canonicalFieldsUpdater)
     {
-        $this->container = $container;
+        $this->passwordUpdater = $passwordUpdater;
+        $this->canonicalFieldsUpdater = $canonicalFieldsUpdater;
     }
 
     /**
@@ -93,12 +82,8 @@ class UserListener implements EventSubscriber
      */
     private function updateUserFields(UserInterface $user)
     {
-        if (null === $this->userManager) {
-            $this->userManager = $this->container->get('fos_user.user_manager');
-        }
-
-        $this->userManager->updateCanonicalFields($user);
-        $this->userManager->updatePassword($user);
+        $this->canonicalFieldsUpdater->updateCanonicalFields($user);
+        $this->passwordUpdater->hashPassword($user);
     }
 
     /**

Tests/Util/CanonicalFieldsUpdaterTest.php
@@ -1,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\UserBundle\Tests\Util;
+
+use FOS\UserBundle\Tests\TestUser;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+
+class CanonicalFieldsUpdaterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CanonicalFieldsUpdater
+     */
+    private $updater;
+    private $usernameCanonicalizer;
+    private $emailCanonicalizer;
+
+    protected function setUp()
+    {
+        $this->usernameCanonicalizer = $this->getMockCanonicalizer();
+        $this->emailCanonicalizer = $this->getMockCanonicalizer();
+
+        $this->updater = new CanonicalFieldsUpdater($this->usernameCanonicalizer, $this->emailCanonicalizer);
+    }
+
+    public function testUpdateCanonicalFields()
+    {
+        $user = new TestUser();
+        $user->setUsername('Username');
+        $user->setEmail('User@Example.com');
+
+        $this->usernameCanonicalizer->expects($this->once())
+            ->method('canonicalize')
+            ->with('Username')
+            ->will($this->returnCallback('strtolower'));
+
+        $this->emailCanonicalizer->expects($this->once())
+            ->method('canonicalize')
+            ->with('User@Example.com')
+            ->will($this->returnCallback('strtolower'));
+
+        $this->updater->updateCanonicalFields($user);
+        $this->assertSame('username', $user->getUsernameCanonical());
+        $this->assertSame('user@example.com', $user->getEmailCanonical());
+    }
+
+    private function getMockCanonicalizer()
+    {
+        return $this->getMockBuilder('FOS\UserBundle\Util\CanonicalizerInterface')->getMock();
+    }
+}