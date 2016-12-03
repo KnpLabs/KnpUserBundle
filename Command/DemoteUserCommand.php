61ba830effd1377438347f373c250089cd3e61e3
Steve Winter <steve@matatirosolutions.co.uk>
2016-12-03 15:38:38 +0100

    Clarifying that role changes don't apply until the user logs out and back in again

--- Command/DemoteUserCommand.php
+++ Command/DemoteUserCommand.php
@@ -46,10 +46,10 @@ EOT
     {
         if ($super) {
             $manipulator->demote($username);
-            $output->writeln(sprintf('User "%s" has been demoted as a simple user.', $username));
+            $output->writeln(sprintf('User "%s" has been demoted as a simple user. This change will not apply until the user logs out and back in again.', $username));
         } else {
             if ($manipulator->removeRole($username, $role)) {
-                $output->writeln(sprintf('Role "%s" has been removed from user "%s".', $role, $username));
+                $output->writeln(sprintf('Role "%s" has been removed from user "%s". This change will not apply until the user logs out and back in again.', $role, $username));
             } else {
                 $output->writeln(sprintf('User "%s" didn\'t have "%s" role.', $username, $role));
             }
