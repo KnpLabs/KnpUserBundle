KnpLabs/KnpUserBundle

commit 8728a6de2b05bd7365f430b352cc41c3c49d80ef

Fix User serialization

Model/User.php
@@ -149,7 +149,8 @@ abstract class User implements UserInterface, GroupableInterface
     /**
      * Serializes the user.
      *
-     * The serialized data have to contain the fields used by the equals method and the username.
+     * The serialized data have to contain the fields used during check for
+     * changes and the id.
      *
      * @return string
      */
@@ -165,6 +166,10 @@ abstract class User implements UserInterface, GroupableInterface
             $this->credentialsExpired,
             $this->enabled,
             $this->id,
+            $this->expiresAt,
+            $this->credentialsExpireAt,
+            $this->email,
+            $this->emailCanonical,
         ));
     }
 
@@ -189,7 +194,11 @@ abstract class User implements UserInterface, GroupableInterface
             $this->locked,
             $this->credentialsExpired,
             $this->enabled,
-            $this->id
+            $this->id,
+            $this->expiresAt,
+            $this->credentialsExpireAt,
+            $this->email,
+            $this->emailCanonical
         ) = $data;
     }
 

commit bb3f06d4242256b8446c73b320f7b01492a08527

Fix BC break in 1.3.7
Fix BC break introduced in 8728a6de2b05bd7365f430b352cc41c3c49d80ef.

Model/User.php
@@ -184,7 +184,7 @@ abstract class User implements UserInterface, GroupableInterface
         $data = unserialize($serialized);
         // add a few extra elements in the array to ensure that we have enough keys when unserializing
         // older data which does not include all properties.
-        $data = array_merge($data, array_fill(0, 2, null));
+        $data = array_merge($data, array_fill(0, 4, null));
 
         list(
             $this->password,