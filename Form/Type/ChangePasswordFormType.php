e2393a8ce10c9604637312c0a9ea5ae97547c1f7
symfonyaml <symfonyaml@users.noreply.github.com>
2017-07-05 10:58:17 +1000

    Fixed use order
    Fixed use order

--- Form/Type/ChangePasswordFormType.php
+++ Form/Type/ChangePasswordFormType.php
@@ -15,8 +15,8 @@ use FOS\UserBundle\Util\LegacyFormHelper;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ChangePasswordFormType extends AbstractType
 {
