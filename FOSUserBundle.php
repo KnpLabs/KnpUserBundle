255d13c02c2fca3f25df4f58b047231286a80191
Ryan Weaver <ryan@thatsquality.com>
2017-12-07 19:59:21 -0500

    Adding a new compiler pass to notify that the session is required

--- FOSUserBundle.php
+++ FOSUserBundle.php
@@ -14,6 +14,7 @@ namespace FOS\UserBundle;
 use Doctrine\Bundle\CouchDBBundle\DependencyInjection\Compiler\DoctrineCouchDBMappingsPass;
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
+use FOS\UserBundle\DependencyInjection\Compiler\CheckForSessionPass;
 use FOS\UserBundle\DependencyInjection\Compiler\InjectRememberMeServicesPass;
 use FOS\UserBundle\DependencyInjection\Compiler\InjectUserCheckerPass;
 use FOS\UserBundle\DependencyInjection\Compiler\ValidationPass;
@@ -35,6 +36,7 @@ class FOSUserBundle extends Bundle
         $container->addCompilerPass(new ValidationPass());
         $container->addCompilerPass(new InjectUserCheckerPass());
         $container->addCompilerPass(new InjectRememberMeServicesPass());
+        $container->addCompilerPass(new CheckForSessionPass());
 
         $this->addRegisterMappingsPass($container);
     }
